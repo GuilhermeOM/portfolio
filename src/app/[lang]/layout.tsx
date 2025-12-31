c331e2f310b84b3ea2285da8daa958e579ee410a
GuilhermeOM <guilhermeomalta0@gmail.com>
2025-12-31 20:24:21 -0300

    fix: next 16 build

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,23 +3,22 @@ import Footer from '@/components/footer/footer'
 import Header from '@/components/header'
 import { Locale } from '@/lib/i18n/i18n-config'
 
-export default async function HomeLayout(
-  props: Readonly<{
-    children: React.ReactNode
-    params: Promise<{ lang: Locale }>
-  }>
-) {
-  const params = await props.params
-
-  const { children } = props
+export default async function HomeLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ lang: string }>
+}) {
+  const { lang } = (await params) as { lang: Locale }
 
   return (
     <div>
-      <Header lang={params.lang} />
+      <Header lang={lang} />
       <main className="pb-16 pt-28">
         <Container>{children}</Container>
       </main>
-      <Footer lang={params.lang} />
+      <Footer lang={lang} />
     </div>
   )
 }
